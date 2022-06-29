54e8c65015dcfdadac5149da04645547335e5c86
tzuge <47162374+tzuge@users.noreply.github.com>
2022-06-29 14:11:46 -0600

    feat(lto-property-tax-app, lto-property-tax-service): stubbing projects for a full stack solution for lto property taxes program.

---
+++ apps/lto-property-tax-app/src/environments/environment.ts
@@ -0,0 +1,11 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// When building for production, this file is replaced with `environment.prod.ts`.
+
+export const environment = {
+  production: false,
+  access: {
+    url: 'https://access-uat.alberta.ca',
+    realm: '6d2d44c9-22b2-4e12-90da-8547480637f6',
+    client_id: 'lto-property-tax-app',
+  },
+};
---
+++ apps/lto-property-tax-service/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'lto-property-tax-service',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/lto-property-tax-service',
+};
